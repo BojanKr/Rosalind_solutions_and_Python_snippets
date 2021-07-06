58908d3150ffa61d53cc7160c8a167284ea4d7c4
bk <bojankrtenic@gmail.com>
2021-07-06 22:25:45 +0200

    Rosalind problem 3

--- 3_Complementing_a_Strand_of_DNA.py
+++ 3_Complementing_a_Strand_of_DNA.py
@@ -1,25 +1,39 @@
-import os
+""" Given: A DNA string s of length at most 1000 bp.
 
-def reverse_complement():
+    Return: The reverse complement sc of s. """
 
-    with open(os.path.join(os.getcwd(), '3_rosalind_revc.txt')) as f:
+
+def main():
+
+    dataset = input('Path to dataset: ')
+
+    with open(dataset, 'r') as f:
         seq = f.readlines()[0]
-        print(seq)
-        reverse_complement_seq = ''
 
         reverse_seq = seq[::-1]
-        print(reverse_seq)
 
-        for nucleotide in reverse_seq:
-            if nucleotide == 'A':
-                reverse_complement_seq = reverse_complement_seq + 'T'
-            elif nucleotide == 'C':
-                reverse_complement_seq = reverse_complement_seq + 'G'
-            elif nucleotide == 'G':
-                reverse_complement_seq = reverse_complement_seq + 'C'
-            elif nucleotide == 'T':
-                reverse_complement_seq = reverse_complement_seq + 'A'
+        reverse = reverse_complement(sequence=reverse_seq)
+        print(reverse)
 
-        print(reverse_complement_seq)
 
-reverse_complement()
+def reverse_complement(sequence):
+
+    """ Finds the reverse complement of a given DNA sequence """
+
+    reverse_complement_seq = ''
+
+    for nucleotide in sequence:
+        if nucleotide == 'A':
+            reverse_complement_seq = reverse_complement_seq + 'T'
+        elif nucleotide == 'C':
+            reverse_complement_seq = reverse_complement_seq + 'G'
+        elif nucleotide == 'G':
+            reverse_complement_seq = reverse_complement_seq + 'C'
+        elif nucleotide == 'T':
+            reverse_complement_seq = reverse_complement_seq + 'A'
+
+    return reverse_complement_seq
+
+
+if __name__ == '__main__':
+    main()
