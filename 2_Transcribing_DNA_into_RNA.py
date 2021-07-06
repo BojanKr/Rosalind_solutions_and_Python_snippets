fd6ef2136cc48b892102986d7837d1de36746a44
bk <bojankrtenic@gmail.com>
2021-07-06 21:07:20 +0200

    Rosalind problem 2

--- 2_Transcribing_DNA_into_RNA.py
+++ 2_Transcribing_DNA_into_RNA.py
@@ -1,18 +1,32 @@
-import os
+""" Given: A DNA string t having length at most 1000 nt.
 
-def transcribe():
+    Return: The transcribed RNA string of t. """
 
-    with open(os.path.join(os.getcwd(), '2_rosalind_rna.txt')) as f:
+def main():
+
+    dataset = input('Path to dataset: ')
+
+    with open(dataset, 'r') as f:
         dna_seq = f.readlines()[0]
-        print(dna_seq)
-        rna_seq = ''
 
-        for nucleotide in dna_seq:
-            if nucleotide is not 'T':
-                rna_seq = rna_seq + nucleotide
-            else:
-                rna_seq = rna_seq + 'U'
+        rna_sequence = transcribe(dna_sequence=dna_seq)
+        print(rna_sequence)
 
-    print(rna_seq)
 
-transcribe()
+def transcribe(dna_sequence):
+
+    """ Transcribes DNA sequence into RNA sequence """
+
+    rna_seq = ''
+
+    for nucleotide in dna_sequence:
+        if nucleotide is not 'T':
+            rna_seq = rna_seq + nucleotide
+        else:
+            rna_seq = rna_seq + 'U'
+
+    return rna_seq
+
+
+if __name__ == '__main__':
+    main()
